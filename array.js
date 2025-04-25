9628fddc1911bd85ec1286776bd20d66384ff649
Olusodo Ayotunde <tundeolusodo@gmail.com>
2025-04-25 08:46:23 +0100

    initial commit

--- array.js
+++ array.js
@@ -1,6 +1,10 @@
+// Arrays store multiple values in one variable
+// Access by index (starts at 0)
+//Arays contain properties
+
 let fruits = ["apple", "banana", "mango"];
 
 fruits.push("orange");
 fruits.shift();
-console.log(fruits[2])
+
 console.log(fruits);
---
+++ forLoop.js
@@ -0,0 +1,23 @@
+// A for loop is a way to repeat a block of code a set number of times.
+//It's useful for working with arrays, lists, or when you need to perform an action multiple times.
+
+/*
+For Loop Syntax
+for (initialization; condition; increment) {
+  code to be executed
+}
+
+*/
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+
+
+let fruits = ["apple", "banana", "mango"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+
