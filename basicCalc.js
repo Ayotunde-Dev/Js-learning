8ce6cde4cf89abd7da9e8038bb3da5c9658a2557
Olusodo Ayotunde <tundeolusodo@gmail.com>
2025-04-29 17:06:44 +0100

    initial commit

--- basicCalc.js
+++ basicCalc.js
@@ -1,5 +1,11 @@
 // Function to calculate arithmetic operations with 2 numbers
 function simpleCalculator(num1, num2, operator) {
+    if (typeof num1 !== 'number') {
+      return false
+    }
+    if (typeof num2 !== 'number') {
+      return false
+    }
     if (operator == '+') {
       return num1 + num2;
     } else if (operator == '-') {
@@ -9,15 +15,18 @@ function simpleCalculator(num1, num2, operator) {
     } else if (operator == '/') {
       return num1 / num2; 
     } else if (operator == '%') {
-        return num1 % num2;
+        return num1 % num2  ;
     } else {
       return "Invalid";
     }
+    
   }
-
-  console.log(simpleCalculator(4, 2, '*')); 
-  console.log(simpleCalculator(4, 2, '%'));
-  console.log(simpleCalculator(4, 2, '-'));
-  console.log(simpleCalculator(4, 2, '+'));
-  console.log(simpleCalculator(4, 2, '/'));
+  console.log(simpleCalculator(8, 2, '+'));
+  console.log(simpleCalculator(5, 2, '-')); 
+  console.log(simpleCalculator(11, 2, '%'));
+  console.log(simpleCalculator(4, 1, '/'));
+  console.log(simpleCalculator(2, 2, '*'));
+  console.log(simpleCalculator(4, false, '+'));
+  console.log(simpleCalculator('dd', 2, '/'));
+  console.log(simpleCalculator(false, false, '*'));
   
